6aaaf5143ab07ed24585b35403bfdb7df8a7459f
Aman <aman.s.chhina@gmail.com>
2021-06-01 00:43:27 -0400

    Completed routing with a makeshift nav bar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import About from "./Pages/About";
 import Home from "./Pages/Home";
 import Meeting from "./Pages/Meeting";
@@ -8,9 +8,24 @@ import "./styles/app.css";
 const App = () => {
     return (
         <Router>
-            <Router path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/meeting" component={Meeting} />
+            <div className="container">
+                <nav>
+                    <ul style={{ display: "inline", marginRight: "2rem" }}>
+                        <Link to="/">Home</Link>
+                    </ul>
+                    <ul style={{ display: "inline", marginRight: "2rem" }}>
+                        <Link to="/about">About</Link>
+                    </ul>
+                    <ul style={{ display: "inline", marginRight: "2rem" }}>
+                        <Link to="/meeting">Meeting</Link>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/about" component={About} />
+                    <Route path="/meeting" component={Meeting} />
+                    <Route path="/" component={Home} />
+                </Switch>
+            </div>
         </Router>
     );
 };
